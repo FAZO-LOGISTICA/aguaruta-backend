39d924c83169b1d5f7fe51c06ebad90223d950b5
FAZO-LOGISTICA <gustavo.oliva.miranda@gmail.com>
2025-08-12 19:52:13 -0400

    Create __init__.py

---
+++ routers/__init__.py
@@ -0,0 +1,11 @@
+# __init__.py
+"""
+Este archivo marca este directorio como un paquete de Python.
+Puede estar vacío o contener inicializaciones de paquete.
+"""
+
+# Si quieres importar algo automáticamente al usar "import routers"
+# puedes hacerlo así:
+# from .entregas import router as entregas_router
+# from .redistribucion import router as redistribucion_router
+# from .rutas_activas import router as rutas_activas_router
